05131b79ae34c81ec5c72719af139c8df39d7239
ffx01 <walters.justin01@gmail.com>
2017-05-05 17:37:47 -0700

    Improved filtering for image tags.

--- main/media/api/resources.py
+++ main/media/api/resources.py
@@ -1,12 +1,23 @@
 from rest_framework.viewsets import ModelViewSet
 from dry_rest_permissions.generics import DRYPermissions
+import django_filters.rest_framework as filters
+from taggit.managers import TaggableManager
 
 from media.models import Image
 from .serializers import ImageSerializer
+
+
+class ImageFilter(filters.FilterSet):
+    owner = filters.NumberFilter(name="owner__id", lookup_expr="exact")
+    tags = filters.CharFilter(name="tags__name", lookup_expr="iexact")
+
+    class Meta:
+        model = Image
+        fields = ('owner', 'articles', 'tags')
 
 
 class ImageViewSet(ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
     permission_classes = (DRYPermissions, )
-    filter_fields = ('owner', 'articles')
+    filter_class = ImageFilter
